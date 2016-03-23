44e21c30e598b066e3ba2d2279a348b092f561a3
Sulabh Bista <sul4bh@gmail.com>
2016-03-23 11:24:43 -0400

    sync from upstream

--- init.zsh
+++ init.zsh
@@ -117,3 +117,5 @@ source "${ZDOTDIR:-$HOME}/.zprezto/aliases"
 
 # Add scripts folder to path
 export PATH=${ZDOTDIR:-$HOME}/.zprezto/scripts:$PATH
+
+source ~/.iterm2_shell_integration.`basename $SHELL`
